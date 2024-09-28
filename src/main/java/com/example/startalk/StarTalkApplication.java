5ef04132e12be4a45456e57de48d065bb8991ac9
LeonovaAV <annaleonova1808@yandex.ru>
2024-09-28 12:18:33 +0300

    create project

---
+++ src/main/java/com/example/startalk/StarTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.startalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/startalk/StarTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
